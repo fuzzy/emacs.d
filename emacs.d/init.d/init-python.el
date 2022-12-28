5c83d84d967127229785c6dae21991e8e4a7cccb
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2022-12-28 16:13:50 -0600

    updated python layer

--- emacs.d/init.d/init-python.el
+++ emacs.d/init.d/init-python.el
@@ -1,7 +1,17 @@
-(use-package anaconda-mode
+(use-package elpy
   :straight t
-  :hook
-  (python-mode-hook . anaconda-mode)
-  (python-mode-hook . anaconda-eldoc-mode))
+  :ensure t
+  :init
+  (elpy-enable))
+
+(add-hook 'elpy-mode-hook (lambda ()
+                            (add-hook 'before-save-hook
+                                      'elpy-black-fix-code nil t)))
+
+;; (use-package anaconda-mode
+;;   :straight t
+;;   :hook
+;;   (python-mode-hook . anaconda-mode)
+;;   (python-mode-hook . anaconda-eldoc-mode))
 
 (provide 'init-python)
